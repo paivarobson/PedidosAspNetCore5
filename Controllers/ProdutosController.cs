0e369948b097ed1f2bc712478765bce89f579c74
Robson Paiva <paivawork@hotmail.com>
2021-06-14 00:48:22 -0300

    Persistência dos dados do Produto no BD

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PedidosAspNetCore5.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PedidosAspNetCore5.Controllers
@@ -7,17 +8,24 @@ namespace PedidosAspNetCore5.Controllers
 
     public class ProdutosController : Controller
     {
+
+        private readonly DataDbContext db;
+
+        public ProdutosController(DataDbContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
-            return base.View(Models.Produto.Listagem);
+            return base.View(db.Produtos.ToList().AsQueryable());
         }
 
         [HttpGet]
         public IActionResult Cadastrar(int? id)
         {
-            if (id.HasValue && Models.Produto.Listagem.Any(u => u.ProdutoId == id))
+            if (id.HasValue && db.Produtos.ToList().AsQueryable().Any(u => u.ProdutoId == id))
             {
-                var produto = Models.Produto.Listagem.Single(u => u.ProdutoId == id);
+                var produto = db.Produtos.ToList().AsQueryable().Single(u => u.ProdutoId == id);
                 return base.View(produto);
             }
             return View();
@@ -26,16 +34,19 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Cadastrar(Produto produto)
         {
-            Models.Produto.Salvar(produto);
+            db.Produtos.Add(produto);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult Alterar(int? id)
         {
-            if (id.HasValue && Models.Produto.Listagem.Any(u => u.ProdutoId == id))
+            if (id.HasValue && db.Produtos.ToList().AsQueryable().Any(u => u.ProdutoId == id))
             {
-                var produto = Models.Produto.Listagem.Single(u => u.ProdutoId == id);
+                var produto = db.Produtos.ToList().AsQueryable().Single(u => u.ProdutoId == id);
+
                 return base.View(produto);
             }
             return View();
@@ -44,17 +55,18 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Alterar(Produto produto)
         {
-            Models.Produto.Salvar(produto);
+            db.Produtos.Update(produto);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
-
 
         [HttpGet]
         public IActionResult Excluir(int? id)
         {
-            if (id.HasValue && Models.Produto.Listagem.Any(u => u.ProdutoId == id))
+            if (id.HasValue && db.Produtos.ToList().AsQueryable().Any(u => u.ProdutoId == id))
             {
-                var produto = Models.Produto.Listagem.Single(u => u.ProdutoId == id);
+                var produto = db.Produtos.ToList().AsQueryable().Single(u => u.ProdutoId == id);
                 return base.View(produto);
             }
             return RedirectToAction("Index");
@@ -63,7 +75,9 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Excluir(Produto produto)
         {
-            Models.Produto.Excluir(produto.ProdutoId);
+            db.Produtos.Remove(produto);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
     }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -12,55 +12,5 @@ namespace PedidosAspNetCore5.Models
         public double Preco { get; set; }
         public ICollection<ItemPedido> ItensPedido { get; set; }
 
-        private static List<Produto> listagem = new List<Produto>();
-        public static IQueryable<Produto> Listagem
-        {
-            get
-            {
-                return listagem.AsQueryable();
-            }
-        }
-
-        static Produto()
-        {
-            Produto.listagem.Add(
-                new Produto { ProdutoId = 1, Descricao = "Maça", DataCadastro = "12/06/2021", Preco = 5.99 });
-            Produto.listagem.Add(
-                new Produto { ProdutoId = 2, Descricao = "Banana", DataCadastro = "12/06/2021", Preco = 4.99 });
-            Produto.listagem.Add(
-                new Produto { ProdutoId = 3, Descricao = "Abacaxi", DataCadastro = "12/06/2021", Preco = 3.99 });
-            Produto.listagem.Add(
-                new Produto { ProdutoId = 4, Descricao = "Azeitona", DataCadastro = "12/06/2021", Preco = 2.99 });
-            Produto.listagem.Add(
-                new Produto { ProdutoId = 5, Descricao = "Acerola", DataCadastro = "12/06/2021", Preco = 1.99 });
-        }
-
-        public static void Salvar(Produto produto)
-        {
-            var produtoExistente = Produto.listagem.Find(u => u.ProdutoId == produto.ProdutoId);
-            if (produtoExistente != null)
-            {
-                produtoExistente.Descricao = produto.Descricao;
-                produtoExistente.Preco = produto.Preco;
-                produtoExistente.DataCadastro = DateTime.Today.ToString("dd/MM/yyyy");
-            }
-            else
-            {
-                int maiorId = Produto.Listagem.Max(u => u.ProdutoId);
-                produto.ProdutoId = maiorId + 1;
-                produto.DataCadastro = DateTime.Today.ToString("dd/MM/yyyy");
-                Produto.listagem.Add(produto);
-            }
-        }
-
-        public static void Excluir(int idProduto)
-        {
-            var produtoExistente = Produto.listagem.Find(u => u.ProdutoId == idProduto);
-            if (produtoExistente != null)
-            {
-                Produto.listagem.Remove(produtoExistente);
-            }
-        }
-
     }
 }
