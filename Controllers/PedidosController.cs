e771c2c20ec79318ed1f278e139444eceb2bc00b
Robson Paiva <paivawork@hotmail.com>
2021-06-14 00:58:36 -0300

    Persistência dos dados do Pedido no BD

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -7,17 +7,23 @@ namespace PedidosAspNetCore5.Controllers
 
     public class PedidosController : Controller
     {
+        private readonly DataDbContext db;
+
+        public PedidosController(DataDbContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
-            return base.View(Models.Pedido.Listagem);
+            return base.View(db.Pedidos.ToList().AsQueryable());
         }
 
         [HttpGet]
         public IActionResult Cadastrar(int? id)
         {
-            if (id.HasValue && Models.Pedido.Listagem.Any(u => u.PedidoId == id))
+            if (id.HasValue && db.Pedidos.ToList().AsQueryable().Any(u => u.PedidoId == id))
             {
-                var pedido = Models.Pedido.Listagem.Single(u => u.PedidoId == id);
+                var pedido = db.Pedidos.ToList().AsQueryable().Single(u => u.PedidoId == id);
                 return base.View(pedido);
             }
             return View();
@@ -26,16 +32,19 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Cadastrar(Pedido pedido)
         {
-            Models.Pedido.Salvar(pedido);
+            db.Pedidos.Add(pedido);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult Alterar(int? id)
         {
-            if (id.HasValue && Models.Pedido.Listagem.Any(u => u.PedidoId == id))
+            if (id.HasValue && db.Pedidos.ToList().AsQueryable().Any(u => u.PedidoId == id))
             {
-                var pedido = Models.Pedido.Listagem.Single(u => u.PedidoId == id);
+                var pedido = db.Pedidos.ToList().AsQueryable().Single(u => u.PedidoId == id);
+
                 return base.View(pedido);
             }
             return View();
@@ -44,17 +53,18 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Alterar(Pedido pedido)
         {
-            Models.Pedido.Salvar(pedido);
+            db.Pedidos.Update(pedido);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
-
 
         [HttpGet]
         public IActionResult Excluir(int? id)
         {
-            if (id.HasValue && Models.Pedido.Listagem.Any(u => u.PedidoId == id))
+            if (id.HasValue && db.Pedidos.ToList().AsQueryable().Any(u => u.PedidoId == id))
             {
-                var pedido = Models.Pedido.Listagem.Single(u => u.PedidoId == id);
+                var pedido = db.Pedidos.ToList().AsQueryable().Single(u => u.PedidoId == id);
                 return base.View(pedido);
             }
             return RedirectToAction("Index");
@@ -63,7 +73,9 @@ namespace PedidosAspNetCore5.Controllers
         [HttpPost]
         public IActionResult Excluir(Pedido pedido)
         {
-            Models.Pedido.Excluir(pedido.PedidoId);
+            db.Pedidos.Remove(pedido);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
     }
--- Models/Pedido/Pedido.cs
+++ Models/Pedido/Pedido.cs
@@ -12,57 +12,5 @@ namespace PedidosAspNetCore5.Models
         public string DataCadastro { get; set; }
         public double Valor { get; set; }
         public ICollection<ItemPedido> Itens { get; set; }
-        private static List<Pedido> listagem = new List<Pedido>();
-        public static IQueryable<Pedido> Listagem
-        {
-            get
-            {
-                return listagem.AsQueryable();
-            }
-        }
-
-        static Pedido()
-        {
-            Produto produto = new Produto();
-
-            Pedido.listagem.Add(
-                new Pedido { PedidoId = 1, /*Fornecedor = "Maça",*/ DataCadastro = "12/06/2021", Valor = 105.99 });
-            Pedido.listagem.Add(
-                new Pedido { PedidoId = 2, /*Fornecedor = "Banana",*/ DataCadastro = "12/06/2021", Valor = 1004.99 });
-            Pedido.listagem.Add(
-                new Pedido { PedidoId = 3, /*Fornecedor = "Abacaxi",*/ DataCadastro = "12/06/2021", Valor = 1003.99 });
-            Pedido.listagem.Add(
-                new Pedido { PedidoId = 4, /*Fornecedor = "Azeitona",*/ DataCadastro = "12/06/2021", Valor = 1002.99 });
-            Pedido.listagem.Add(
-                new Pedido { PedidoId = 5, /*Fornecedor = "Acerola",*/ DataCadastro = "12/06/2021", Valor = 1001.99 });
-        }
-
-        public static void Salvar(Pedido pedido)
-        {
-            var pedidoExistente = Pedido.listagem.Find(u => u.PedidoId == pedido.PedidoId);
-            if (pedidoExistente != null)
-            {
-                // pedidoExistente.Fornecedor = pedido.Fornecedor;                
-                pedidoExistente.DataCadastro = DateTime.Today.ToString("dd/MM/yyyy");
-                pedidoExistente.Valor = pedido.Valor;
-            }
-            else
-            {
-                int maiorId = Pedido.Listagem.Max(u => u.PedidoId);
-                pedido.PedidoId = maiorId + 1;
-                pedido.DataCadastro = DateTime.Today.ToString("dd/MM/yyyy");
-                Pedido.listagem.Add(pedido);
-            }
-        }
-
-        public static void Excluir(int idPedido)
-        {
-            var pedidoExistente = Pedido.listagem.Find(u => u.PedidoId == idPedido);
-            if (pedidoExistente != null)
-            {
-                Pedido.listagem.Remove(pedidoExistente);
-            }
-        }
-
     }
 }
